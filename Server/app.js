59004049b92e83a5427da06e307bb390e5564737
barryos112 <barryos112@gmail.com>
2022-10-21 15:31:33 +0100

    Input API data to html

--- Server/app.js
+++ Server/app.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const PORT = 3000;
@@ -12,25 +12,28 @@ app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server is now listening on port ${PORT}`);
-})
+});
 
 // ------------
 // Controllers
 // ------------
-app.get('/', async (req, res) => {
-  await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Dublin&appid=${API_KEY}&units=metric`)
-  .then((response) => {
-    res.send(response.data);
-  });
+app.get("/", async (req, res) => {
+  await axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/weather?q=Dublin&appid=${API_KEY}&units=metric`
+    )
+    .then((response) => {
+      res.send(response.data);
+    });
 });
 
-
-app.get('/weather', async (req, res) => {
-  let city = req.query.city
-  await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
-  .then((response) => {
-    res.send(response.data.weather[0].main);
-  });
+app.get("/weather", async (req, res) => {
+  let city = req.query.city;
+  await axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    )
+    .then((response) => {
+      res.send(response.data);
+    });
 });
-
-
--- client/components/search.js
+++ client/components/search.js
@@ -1,27 +1,25 @@
 const Search = {
   template: `
-  <div id="wrap">
-  <form action="" autocomplete="on">
-    <input id="search" name="search" type="text" placeholder="Search for a city"/>
-    <input id="search_submit" value="Rechercher" type="submit" v-on:click="registerCity()"/>
-  </form>
-  </div>
-
-
   <div class="information-container">
+    <div>
+      <h1> {{ currCity }} </h1>
+      <input id="search-input" placeholder="Search a city"/>
+      <button v-on:click="searchCity()"> Go </button>
+    </div>
+
     <div class="information-table-wrapper">
         <table class="information-table">
           <tr class="information-table-row">
             <th class="information-table-header">Temperature</th>
-            <td class="information-table-data">20</td>
+            <td class="information-table-data"> {{ temperature }}°C</td>
           </tr>
           <tr class="information-table-row">
-            <th class="information-table-header">Rainfall</th>
-            <td class="information-table-data">2mm</td>
+          <th class="information-table-header">Weather <span v-if="isRaining" style="font-weight: 300"> <br/> Bring an umbrella! </span> </th>
+          <td class="information-table-data">{{ weather }} </td>
           </tr>
           <tr class="information-table-row">
             <th class="information-table-header">Wind Speed</th>
-            <td class="information-table-data">20 km/h</td>
+            <td class="information-table-data">{{ wind_speed }} km/h</td>
           </tr>
           <tr class="information-table-row">
             <th class="information-table-header">Air Pollution</th>
@@ -34,23 +32,35 @@ const Search = {
 
   data() {
     return {
-      message: "Test",
       currCity: "Dublin",
+      wind_speed: 5,
+      temperature: 8,
+      weather: "Clouds",
+      isRaining: false,
     };
   },
 
   methods: {
-    async registerCity() {
-      let city = document.querySelector("search_submit").value;
+    async searchCity() {
+      let city = document.querySelector("#search-input").value;
+
       await fetch(`http://127.0.0.1:3000/weather?city=${city}`)
         .then((res) => {
-          const data = res.text();
+          const data = res.json();
           return data;
         })
         .then((data) => {
           console.log(data);
-          this.message = data;
           this.currCity = city;
+          this.wind_speed = data.wind.speed;
+          this.temperature = data.main.temp;
+          this.weather = data.weather[0].main;
+
+          if (this.weather == "Rain") {
+            this.isRaining = true;
+          } else {
+            this.isRaining = false;
+          }
         });
     },
   },
